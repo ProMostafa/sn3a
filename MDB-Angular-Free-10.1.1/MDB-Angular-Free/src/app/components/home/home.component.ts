5401085f734a139be6ff7d22f4392f789f054572
Hossam-Soliman-1864 <hossammohamed28394@yahoo.com>
2020-12-21 20:53:11 +0200

    login

--- MDB-Angular-Free-10.1.1/MDB-Angular-Free/src/app/components/home/home.component.ts
+++ MDB-Angular-Free-10.1.1/MDB-Angular-Free/src/app/components/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Icontact } from '../../views/interface/icontact';
+import { ContactService } from '../../services/contact.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+contact:Icontact;
+  constructor(private _ApiContact:ContactService,private _router:Router) { 
+    this.contact={
+      id:null,
+      name:" ",
+      phone:null,
+      email:" ",
+     message:" ",
+      company:" "
 
-  constructor() { }
+    }
+
+  }
 
   ngOnInit(): void {
   }
+  Contact(){
+    this._ApiContact.ContactUser(this.contact).subscribe(
+      (data)=>this._router.navigateByUrl('Contact'),
+      (err)=>console.log(err)
+  
+  
+    )
+  
+  }
+  
 
 }
+
+
+  
+
--- MDB-Angular-Free-10.1.1/MDB-Angular-Free/src/app/components/navbar/navbar.component.ts
+++ MDB-Angular-Free-10.1.1/MDB-Angular-Free/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 // import {FormControl, FormGroup, Validators} from "@angular/forms";
-import { Ilogin } from '../../views/interface/ilogin';
-import { LoginService } from '../../services/login.service';
-import { Icustomer } from '../../views/interface/icustomer';
-import { CustomerService } from '../../services/customer.service';
-import { Router } from '@angular/router';
+// import { Ilogin } from '../../views/interface/ilogin';
+// import { LoginService } from '../../services/login.service';
+// import { Icustomer } from '../../views/interface/icustomer';
+// import { CustomerService } from '../../services/customer.service';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -12,39 +12,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  user:Ilogin;
-  customer:Icustomer;
+  // user:Ilogin;
+  // customer:Icustomer;
+  
+constructor(){}
+  // constructor(private _ApiCustomer:CustomerService,private _ApiUser:LoginService,private _router:Router) { 
+    // this.user={  
+    //   email: " ",
+    //   password:null
+    //  };
+    //  this.customer={  
+    //   email: " ",
+    //   username:" ",
+    //   address: " " ,
+    //   phone:null,
+    //   img:" ",
+    //   is_technical:false,
+    //   password:null
+    //  };
   
 
-  constructor(private _ApiCustomer:CustomerService,private _ApiUser:LoginService,private _router:Router) { 
-    this.user={  
-      email: " ",
-      password:null
-     };
-     this.customer={  
-      email: " ",
-      username:" ",
-      address: " " ,
-      phone:null,
-      img:" ",
-      is_technical:false,
-      password:null
-     };
-  }
-
   ngOnInit(): void { }
-  Login(){
-    this._ApiUser.loginUser(this.user).subscribe(
-      (data)=>this._router.navigateByUrl('/Login'),
-      (err)=>console.log(err)
+  // Login(){
+  //   this._ApiUser.loginUser(this.user).subscribe(
+  //     (data)=>this._router.navigateByUrl('/Login'),
+  //     (err)=>console.log(err)
 
-    )
-  }
-  CreateAccount(){
-    this._ApiCustomer.insertCustomer(this.customer).subscribe(
-      (data)=>this._router.navigateByUrl('/Customer'),
-      (err)=>console.log(err)
-    )
-   }
+  //   )
+  // }
+  // CreateAccount(){
+  //   this._ApiCustomer.insertCustomer(this.customer).subscribe(
+  //     (data)=>this._router.navigateByUrl('/Customer'),
+  //     (err)=>console.log(err)
+  //   )
+  //  }
 
 }
