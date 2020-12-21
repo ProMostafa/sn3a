ecd7d8e82c4f31a9bf211dbd1fa28f9f1b631efe
ProMostafa <promostafaeladawy@gmail.com>
2020-12-22 01:51:46 +0200

    user_Authentication_Done

--- backend/homeassistant/settings.py
+++ backend/homeassistant/settings.py
@@ -59,8 +59,12 @@ ROOT_URLCONF = 'homeassistant.urls'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'NON_FIELD_ERRORS_KEY' : 'error',
 }
 
 TEMPLATES = [
