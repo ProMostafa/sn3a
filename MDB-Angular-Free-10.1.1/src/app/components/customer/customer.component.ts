f7214d64a40c34da4312739f612c33262a2afba4
muhammedashraf9244 <hom9244.9244@gmail.com>
2021-01-01 19:35:41 +0200

    update customer

--- MDB-Angular-Free-10.1.1/src/app/components/customer/customer.component.ts
+++ MDB-Angular-Free-10.1.1/src/app/components/customer/customer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Icustomer } from '../../views/interface/icustomer';
+import { Iorder } from '../../views/interface/iorder';
 
 @Component({
   selector: 'app-customer',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
-
-  constructor() { }
+Customer:Icustomer;
+OrderCustomerList:Iorder[];
+  constructor() { 
+  
+  }
 
   ngOnInit(): void {
+    this.Customer={ 
+      address:"بنى سويف",
+      email:"hom9244.9244@gmail.com",
+      password:"8888",
+      phone:"01098355960",
+      username:"محمد_أشرف",
+      first_name:"محمد",
+      last_name:"اشرف",
+      job:"مصمم ويب",
+    }
+    this.OrderCustomerList=[{
+      status:true,
+      date:11,
+      description:"خدمة سباكة",
+      total_cost:2563,
+      technical:2,
+      service:2
+
+    }]
   }
 
 }
